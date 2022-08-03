6840996b13f7e35c1319c43367fb27ff9d6a6942
Florent Lebeau <florent.lebeau@arm.com>
2022-08-03 11:59:30 +0200

    Don't specify process count

--- test.sh
+++ test.sh
@@ -30,7 +30,7 @@ patch -s -p 1 < ${ROOT_DIR}/test/patches/asan.patch
 echo "Testing C version"
 pushd src/C
 juLog -name="build" -class="c_dbg" make
-mpirun --oversubscribe -n 4 ./mmult 2>&1 | tee app.out
+mpirun ./mmult 2>&1 | tee app.out
 juLog -name="run_buffer_overflow" -class="c_dbg" check_output "AddressSanitizer: heap-buffer-overflow"
 #juLog -name="run_stack0" -class="c_dbg" check_output "mmult.c:67"
 #juLog -name="run_stack1" -class="c_dbg" check_output "mmult.c:164"
@@ -39,7 +39,7 @@ popd
 echo "Testing Fortran version"
 pushd src/F90
 juLog -name="build" -class="fortran_dbg" make
-mpirun --oversubscribe -n 4 ./mmult 2>&1 | tee app.out
+mpirun ./mmult 2>&1 | tee app.out
 juLog -name="run_buffer_overflow" -class="fortran_dbg" check_output "AddressSanitizer: heap-buffer-overflow"
 #juLog -name="run_stack0" -class="fortran_dbg" check_output "mmult.F90:78"
 #juLog -name="run_stack1" -class="fortran_dbg" check_output "mmult.F90:17"
@@ -48,9 +48,9 @@ popd
 echo "Testing Python version"
 pushd src/Py
 juLog -name="build" -class="py_dbg" make
-mpirun --oversubscribe -n 4 python3 ./mmult.py -k C &> app.out
+mpirun python3 ./mmult.py -k C &> app.out
 juLog -name="run_c_buffer_overflow" -class="py_dbg" check_output "AddressSanitizer: heap-buffer-overflow"
-mpirun --oversubscribe -n 4 python3 ./mmult.py -k F90 &> app.out
+mpirun python3 ./mmult.py -k F90 &> app.out
 juLog -name="run_f90_buffer_overflow" -class="py_dbg" check_output "AddressSanitizer: heap-buffer-overflow"
 popd
 
