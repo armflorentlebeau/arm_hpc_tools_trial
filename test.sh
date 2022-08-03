e4a799870399e7cc11e2b84f8aeeeb6be784268d
Florent Lebeau <florent.lebeau@arm.com>
2022-08-03 13:39:20 +0200

    Split tests by version

--- test.sh
+++ test.sh
@@ -3,17 +3,11 @@
 ROOT_DIR=${PWD}
 TMP_DIR=${ROOT_DIR}/tmp_test
 
-if [ ! -f ${TMP_DIR} ]; then
-  rm -rf ${TMP_DIR}
-fi
-mkdir ${TMP_DIR} && cd ${TMP_DIR}
-cp -r ${ROOT_DIR}/src .
 
-# Requires https://github.com/manolo/shell2junit in shell2junit folder
-git clone https://github.com/manolo/shell2junit
-. shell2junit/sh2ju.sh
+usage () {
+  echo "$0 [C | F90 | Py]"
+}
 
-juLogClean
 
 check_output () { # $1-string
   #check if $1 is found in app.out
@@ -24,67 +18,78 @@ check_output () { # $1-string
   fi
 }
 
-# This hangs with github docker container
-# Enable ASAN
-#patch -s -p 1 < ${ROOT_DIR}/test/patches/asan.patch
 
-#echo "Testing C version"
-#pushd src/C
-#juLog -name="build" -class="c_dbg" make
-#mpirun ./mmult 2>&1 | tee app.out
-#juLog -name="run_buffer_overflow" -class="c_dbg" check_output "AddressSanitizer: heap-buffer-overflow"
-#juLog -name="run_stack0" -class="c_dbg" check_output "mmult.c:67"
-#juLog -name="run_stack1" -class="c_dbg" check_output "mmult.c:164"
-#popd
-
-#echo "Testing Fortran version"
-#pushd src/F90
-#juLog -name="build" -class="fortran_dbg" make
-#mpirun ./mmult 2>&1 | tee app.out
-#juLog -name="run_buffer_overflow" -class="fortran_dbg" check_output "AddressSanitizer: heap-buffer-overflow"
-#juLog -name="run_stack0" -class="fortran_dbg" check_output "mmult.F90:78"
-#juLog -name="run_stack1" -class="fortran_dbg" check_output "mmult.F90:17"
-#popd
-
-#echo "Testing Python version"
-#pushd src/Py
-#juLog -name="build" -class="py_dbg" make
-#mpirun python3 ./mmult.py -k C &> app.out
-#juLog -name="run_c_buffer_overflow" -class="py_dbg" check_output "AddressSanitizer: heap-buffer-overflow"
-#mpirun python3 ./mmult.py -k F90 &> app.out
-#juLog -name="run_f90_buffer_overflow" -class="py_dbg" check_output "AddressSanitizer: heap-buffer-overflow"
-#popd
+test_c () {
+  echo "Testing C version"
+  pushd src/C
+  juLog -name="build" make
+  mpirun ./mmult 2>&1 | tee app.out
+  juLog -name="run" check_output "0: Done."
+  juLog -name="results" diff -q res_*.mat ${ROOT_DIR}/test/ref/ref_C.mat
+  popd
+}
 
 
-patch -s -p 1 < ${ROOT_DIR}/test/patches/fix.patch
+test_f90 () {
+  echo "Testing Fortran version"
+  pushd src/F90
+  juLog -name="f_build" make
+  mpirun ./mmult 2>&1 | tee app.out
+  juLog -name="f_run" check_output "0 : Done."
+  juLog -name="f_results" diff -q res_*.mat ${ROOT_DIR}/test/ref/ref_F90.mat
+  popd
+}
 
-echo "Testing C version"
-pushd src/C
-juLog -name="build" -class="c_dbg" make
-mpirun ./mmult 2>&1 | tee app.out
-juLog -name="run" -class="c_dbg" check_output "0: Done."
-juLog -name="results" -class="c_dbg" diff -q res_*.mat ${ROOT_DIR}/test/ref/ref_C.mat
-popd
 
-echo "Testing Fortran version"
-pushd src/F90
-juLog -name="build" -class="f_dbg" make
-mpirun ./mmult 2>&1 | tee app.out
-juLog -name="run" -class="f_dbg" check_output "0: Done."
-juLog -name="results" -class="f_dbg" diff -q res_*.mat ${ROOT_DIR}/test/ref/ref_F90.mat
-popd
+test_py () {
+  echo "Testing Py version"
+  pushd src/Py
+  juLog -name="py_build" make
+  mpirun python3 ./mmult.py -k C 2>&1 | tee app.out
+  juLog -name="py_run_c" check_output "0: Done."
+  juLog -name="py_results_c" diff -q res_*.mat ${ROOT_DIR}/test/ref/ref_Py.mat
+  mpirun python3 ./mmult.py -k F90 2>&1 | tee app.out
+  juLog -name="py_run_f" check_output "0: Done."
+  juLog -name="py_results_f" diff -q res_*.mat ${ROOT_DIR}/test/ref/ref_Py.mat
+  popd
+}
 
-echo "Testing Py version"
-pushd src/Py
-juLog -name="build" -class="py_dbg" make
-mpirun python3 ./mmult.py -k C 2>&1 | tee app.out
-juLog -name="run_c" -class="py_dbg" check_output "0: Done."
-juLog -name="results_c" -class="py_dbg" diff -q res_*.mat ${ROOT_DIR}/test/ref/ref_Py.mat
-mpirun python3 ./mmult.py -k F90 2>&1 | tee app.out
-juLog -name="run_f" -class="py_dbg" check_output "0: Done."
-juLog -name="results_f" -class="py_dbg" diff -q res_*.mat ${ROOT_DIR}/test/ref/ref_Py.mat
-popd
 
+setup () {
+  if [ ! -f ${TMP_DIR} ]; then
+    rm -rf ${TMP_DIR}
+  fi
+  mkdir ${TMP_DIR} && cd ${TMP_DIR}
+  cp -r ${ROOT_DIR}/src .
+
+  # Requires https://github.com/manolo/shell2junit in shell2junit folder
+  git clone https://github.com/manolo/shell2junit
+. shell2junit/sh2ju.sh
+
+  juLogClean
+
+  patch -s -p 1 < ${ROOT_DIR}/test/patches/fix.patch
+}
+
+
+case $1 in 
+  C)
+    setup
+    test_c
+    ;;
+  F90)
+    setup
+    test_f
+    ;;
+  Py)
+    setup
+    test_py
+    ;;
+  *)
+    usage
+    ;;
+esac
 
 cd $ROOT_DIR
+exit
 
